637f282efee27159b1afdce9d6fb69e18c78890e
gekyxme <gxkyume.7@gmail.com>
2023-05-20 02:22:32 +0530

    feat: team deflation

---
+++ src/pages/api/team/delete.ts
@@ -0,0 +1,57 @@
+import { PrismaClient } from "@prisma/client";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "~/server/auth";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const prisma = new PrismaClient();
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  //const session = await getServerSession(req, res, authOptions);
+  const session = "session";
+  if (session) {
+    const { teamId } = req.body;
+   //const userId = session.user.id;
+   const userId = "clhtez2xz0006ld08a6tsn8y8"; //For testing
+
+    try {
+      const team = await prisma.team.findUnique({
+        where: { id: teamId },
+        include: { members: true },
+      });
+
+      if (!team) {
+        return res.status(404).json("Team doesn't exist");
+      }
+
+      // Check if the current user is the team leader
+      if (team.members.some((member) => member.id === userId && member.leader)) {
+        // Delete the team
+        await prisma.team.delete({
+          where: { id: teamId },
+        });
+
+        // Remove the team ID from the members' records
+        await prisma.user.updateMany({
+          where: { teamId },
+          data: { teamId: null },
+        });
+
+        // Remove the user's leader status
+        await prisma.user.update({
+          where: { id: userId },
+          data: { leader: false },
+        })
+
+        return res.status(200).json("Team deleted successfully");
+      }
+
+      return res.status(403).json("Only the team leader can delete the team");
+    } catch (error) {
+      console.error("Error deleting team:", error);
+      return res.status(500).json("Internal server error");
+    }
+  } else {
+    // Not Signed in
+    return res.status(401).end();
+  }
+};
