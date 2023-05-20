d1e57fb569ef06993b2d82d73c6726bb4a201cff
GARV JAIN <125888777+garv-jain1@users.noreply.github.com>
2023-05-20 21:40:43 +0530

    feat: fixed type errors

--- src/pages/api/team/confirm.ts
+++ src/pages/api/team/confirm.ts
@@ -4,10 +4,14 @@ import { authOptions } from "~/server/auth";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 const prisma = new PrismaClient();
+interface ExtendedNextApiRequest extends NextApiRequest {
+  body: {
+    teamId: string;
+  };
+}
 
-
-
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+const handler = async (req: ExtendedNextApiRequest, res: NextApiResponse) => {
+  
   const session = await getServerSession(req, res, authOptions);
 
   if (session) {
@@ -19,20 +23,20 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       });
 
       if (!team) {
-        return res.status(404).json("Team doesn't exist");
+        return res.status(404).json({ message: "Team doesn't exist" });
       }
 
-      return res.status(200).json({message:team.name});
+      return res.status(200).json({ name: team.name });
     }
     
 
      catch (error) {
       console.error("Error finding team:", error);
-      return res.status(500).json("Internal server error");
+      return res.status(500).json({ message: "Internal Server error" });
     }
   } else {
     // Not Signed in
-    return res.status(401).send("Unauthorized");
+    return res.status(401).json({ message: "Unauthorized" });
   }
 };
 
