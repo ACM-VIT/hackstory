5d770a18dc75d0b47718092012d1a00758922bd9
adityak840 <khandelwal.aditya840@gmail.com>
2023-05-20 12:33:08 +0530

    feat: Folder Restructing & Handlers

---
+++ src/handlers/getHandlers.ts
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import Cookies from 'js-cookie';
+
+const getHandlers = async (URL: string, protect: boolean) => {
+    const headers = {
+        'Content-Type': 'application/json',
+        Authorization: '',
+        'ngrok-skip-browser-warning': true,
+    };
+    if (protect) headers.Authorization = `Bearer ${Cookies.get('token')}`;
+    const response: any = {
+        status: 0,
+        data: '',
+    };
+    await axios
+        .get(URL, { headers })
+        .then((res) => {
+            response.status = 1;
+            response.data = res.data;
+        })
+        .catch((err) => {
+            response.status = 0;
+            response.data = err.response.data;
+        });
+    return response;
+};
+
+export default getHandlers;
