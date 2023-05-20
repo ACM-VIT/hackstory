e6404ceeaee13aefadeed0e44a6d57c495996eb7
Realitylion <dhanushbaalaji123@gmail.com>
2023-05-20 22:23:28 +0530

    Feat: added route listTeamNames

---
+++ src/pages/api/team/listTeamNames.ts
@@ -0,0 +1,26 @@
+import { prisma } from "~/server/db";
+import { type NextApiRequest, type NextApiResponse } from "next";
+import { getServerSession } from "next-auth";
+import { authOptions } from "~/server/auth";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const session = await getServerSession(req, res, authOptions);
+  if (session) {
+    const teamNames = await prisma.team.findMany({
+      select: {
+        name: true,
+      },
+    });
+
+    if (teamNames) {
+      return res.status(200).send(teamNames);
+    } else {
+      return res.status(404).json({ message: "No teams were found" });
+    }
+  } else {
+    return res.status(401).json({ message: "User not in session" });
+  }
+}
