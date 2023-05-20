5fc0a1a981b9ac40620c82254d44aa3afe1cd08f
skizzle010 <raiutkarsh3219@gmail.com>
2023-05-20 16:03:10 +0530

    feat: join team

--- src/pages/api/team/join.ts
+++ src/pages/api/team/join.ts
@@ -1,71 +1,59 @@
-import {PrismaClient} from "@prisma/client"
-import express from "express"
-import { NextApiRequest,NextApiResponse } from "next"
+import { PrismaClient } from "@prisma/client";
+import express from "express";
+import { NextApiRequest, NextApiResponse } from "next";
 
+const prisma = new PrismaClient();
 
-const prisma = new PrismaClient()
+export default async function joinhandler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { team_code, userId } = req.body;
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+    const team = await prisma.team.findUnique({
+      where: {
+        code: team_code,
+      },
+    });
 
-export default async function joinhandler(req: NextApiRequest, res: NextApiResponse) {
-    const {team_code,userId} = req.body;
-    try{
-        const user = await prisma.user.findUnique({
-            where:{
-                id:userId
-            }
-        })
-        const team = await prisma.team.findUnique({
-            where:{
-                code:team_code
-            }
-        })
-
-        if(!user || !team){
-            return res.status(404).json({
-                message:"User or Team not found"
-            })
-        }
-
-        
-
-        // if(team.members.length>=3){
-        //     return res.status(400).json({
-        //         message:"Team is full"
-        //     })
-        // }
-
-        await prisma.team.update({
-            where:{
-                code:team_code
-            },
-            data:{
-                members:{
-                    connect:{
-                        id:userId
-                    }
-                }
-            },
-            include:{
-                members:true
-            }
-
-        })
-        
-        return res.status(200).json({
-            message:"Successfully joined team",
-            team
-        })
-
-    }catch(err)
-    {
-        console.error(err)
-        return res.status(500).json({
-            message:"Internal Server Error"
-        })
+    if (!user || !team) {
+      return res.status(404).json({
+        message: "User or Team not found",
+      });
     }
+
+    // if(team.members.length>=3){
+    //     return res.status(400).json({
+    //         message:"Team is full"
+    //     })
+    // }
+
+    await prisma.team.update({
+      where: {
+        id: team.id,
+      },
+      data: {
+        members: {
+          connect: {
+            id: user.id,
+          },
+        },
+      },
+    });
+
+    return res.status(200).json({
+      message: "Successfully joined team",
+      team: team,
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
 }
-
-
-
-
-
-
