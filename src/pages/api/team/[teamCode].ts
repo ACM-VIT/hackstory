b08d4669d81365ef501270618638351474018908
Realitylion <dhanushbaalaji123@gmail.com>
2023-05-20 12:43:45 +0530

    Fix: Added response codes and messages in view team details handler

--- src/pages/api/team/[teamCode].ts
+++ src/pages/api/team/[teamCode].ts
@@ -9,14 +9,13 @@ export default async function handler (
 ) {
     const prisma = new PrismaClient();
     const session = await getServerSession(req, res, authOptions);
-    
+
     if(session) {
         const curUrl = req.url;
         let teamCode : string | undefined;
         if (typeof curUrl === 'string') {
             teamCode = curUrl.substring(curUrl.lastIndexOf('/') + 1); // getting teamCode from url
         }
-        console.log(teamCode);
 
         // fetching info of team using teamCode
         const teamInfo = await prisma.team.findFirst({
@@ -32,9 +31,15 @@ export default async function handler (
                 code : teamCode
             }
         })
-        return res.send(teamInfo);
+
+        if (teamInfo) {
+            return res.status(200).send(teamInfo);
+        } else {
+            return res.status(404).json("Team code does not correspond to an existing team")
+        }
+
     } else {
-        res.status(401);
+        res.status(401).json("User not in session");
     }
     res.end();
 }
