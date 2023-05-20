a227972c9078b2d68154fbe69d6b3ff7a6136aaa
skizzle010 <raiutkarsh3219@gmail.com>
2023-05-20 16:39:25 +0530

    fix : session_id

--- src/pages/api/team/join.ts
+++ src/pages/api/team/join.ts
@@ -1,59 +1,86 @@
 import { PrismaClient } from "@prisma/client";
-import express from "express";
-import { NextApiRequest, NextApiResponse } from "next";
+import { type Session } from "next-auth";
+import { type NextApiRequest, type NextApiResponse } from "next";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "~/server/auth";
 
 const prisma = new PrismaClient();
 
+interface ExtendedNextApiRequest extends NextApiRequest {
+  body: {
+    teamCode: string;
+  };
+}
+
 export default async function joinhandler(
-  req: NextApiRequest,
+  req: ExtendedNextApiRequest,
   res: NextApiResponse
 ) {
-  const { team_code, userId } = req.body;
+  const session: Session | null = await getServerSession(req, res, authOptions);
+  const { teamCode } = req.body;
   try {
-    const user = await prisma.user.findUnique({
-      where: {
-        id: userId,
-      },
-    });
-    const team = await prisma.team.findUnique({
-      where: {
-        code: team_code,
-      },
-    });
-
-    if (!user || !team) {
-      return res.status(404).json({
-        message: "User or Team not found",
+    if (session) {
+      const user = await prisma.user.findUnique({
+        where: {
+          id: session.user.id,
+        },
       });
-    }
 
-    // if(team.members.length>=3){
-    //     return res.status(400).json({
-    //         message:"Team is full"
-    //     })
-    // }
+      if (!user) {
+        return res.status(404).json({
+          message: "User not found",
+        });
+      }
 
-    await prisma.team.update({
-      where: {
-        id: team.id,
-      },
-      data: {
-        members: {
-          connect: {
-            id: user.id,
+      if (user.teamId) {
+        return res.status(400).json({
+          message: "You are already in a team",
+        });
+      }
+
+      const team = await prisma.team.findUnique({
+        where: {
+          code: teamCode,
+        },
+        include: {
+          members: true,
+        },
+      });
+
+      if (!team) {
+        return res.status(404).json({
+          message: "Team not found",
+        });
+      }
+
+      if (team.members.length >= 3) {
+        return res.status(400).json({
+          message: "Team is full",
+        });
+      }
+
+      await prisma.team.update({
+        where: {
+          id: team.id,
+        },
+        data: {
+          members: {
+            connect: {
+              id: user.id,
+            },
           },
         },
-      },
-    });
+      });
 
-    return res.status(200).json({
-      message: "Successfully joined team",
-      team: team,
-    });
+      return res.status(200).json({
+        message: "Successfully joined team",
+        team: team,
+      });
+    } else {
+      // Not Logged in
+      res.status(401).send({ message: "You are not authenticated" });
+    }
   } catch (err) {
-    console.error(err);
-    return res.status(500).json({
-      message: "Internal Server Error",
-    });
+    res.status(500).send({ err: err, message: "Something went wrong" });
   }
 }
