7cdfcbaf361014e558cd6f17deed6f1401d02f99
Your Name <pavdarsh@gmail.com>
2023-05-20 15:16:18 +0530

    whitespace

--- src/pages/api/team/create.ts
+++ src/pages/api/team/create.ts
@@ -14,41 +14,41 @@ export function generateCode(): string {
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if(req.method === 'POST'){
-  const prisma = new PrismaClient();
-  const {name} = req.body;
-  const session = await getServerSession(req, res, authOptions);
-  if (session) {
-    const userId: string = session.user.id;
-    const data = await prisma.user.findUnique({ where: { id: userId } });
+    const prisma = new PrismaClient();
+    const {name} = req.body;
+    const session = await getServerSession(req, res, authOptions);
+    if (session) {
+      const userId: string = session.user.id;
+      const data = await prisma.user.findUnique({ where: { id: userId } });
   
-  if (data) {
-    if (!data.teamId) {
-      data.leader = true;
+      if (data) {
+        if (!data.teamId) {
+          data.leader = true;
       
-        const teamCode = generateCode();
-        const newTeam = await prisma.team.create({
-          data: {
-            name: name,
-            code: teamCode,
-          },
-        });
-        return res.status(200).json("New team created");
-      } 
-      else {
-        return res.status(400).json("User already has a team");
+          const teamCode = generateCode();
+          const newTeam = await prisma.team.create({
+            data: {
+              name: name,
+              code: teamCode,
+            },
+          });
+          return res.status(200).json("New team created");
+        } 
+        else {
+          return res.status(400).json("User already has a team");
+        }
       }
+      else{
+        return res.status(400).json("user does not exist")
+      }
+      console.log("Session", JSON.stringify(session, null, 2));
     }
-    else{
-      return res.status(400).json("user does not exist")
+    else {
+      // Not Signed in
+      return res.status(401).end();
     }
-    console.log("Session", JSON.stringify(session, null, 2));
   }
-  else {
-    // Not Signed in
- return res.status(401).end();
-}
-}
-else{
-  res.status(500).json("send a different request")
-}
+  else{
+    res.status(500).json("send a different request")
+  }
 }
