bc47fb262a9217be88e2d889e99bfaf5cd096eeb
Arjun Khanna <arjunkhanna0108@gmail.com>
2023-05-23 13:08:10 +0530

    feat: click colours in about-clubs

--- src/components/Clubs/ClubDrop.tsx
+++ src/components/Clubs/ClubDrop.tsx
@@ -1,22 +1,37 @@
 import React from "react";
 import { useState } from "react";
 
-export const ClubDrop = (props) => {
+export const ClubDrop = ({ title, num }) => {
 	const [open, setOpen] = useState(false);
 
 	return (
 		<div>
-			<div className="text-left text-[#A7A9BE]">
+			<div className="text-left ">
 				<div
-					className="flex font-semibold text-2xl justify-between pb-2 mr-20 border-b border-[#A7A9BE] "
+					className={
+						"flex font-semibold text-2xl justify-between pb-2 mr-20 border-b " +
+						(open ? "" : "text-[#A7A9BE] border-[#A7A9BE]") +
+						(num == 1 && open
+							? "text-[#FFCCDE] border-[#FFCCDE]"
+							: "") +
+						(num == 2 && open
+							? "text-[#007656] border-[#007656]"
+							: "") +
+						(num == 3 && open
+							? "text-[#FABF29] border-[#FABF29]"
+							: "") +
+						(num == 4 && open
+							? "text-[#F54E26] border-[#F54E26]"
+							: "")
+					}
 					onClick={() => setOpen(!open)}
 				>
-					<h3 className="">{props.title}</h3>
+					<h3>{title}</h3>
 					<span className={"" + (open ? "hidden" : "")}>+</span>
 					<span className={"" + (open ? "" : "hidden")}>-</span>
 				</div>
 				{open && (
-					<p className="pt-8 mr-20">
+					<p className="pt-8 mr-20 text-[#FFF6E8]">
 						Lorem ipsum dolor sit amet consectetur adipisicing elit.
 						Atque ab odit distinctio. Nihil tempore molestias
 						assumenda error eos illo repellendus!
--- src/components/Clubs/Clubs.tsx
+++ src/components/Clubs/Clubs.tsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import { ClubDrop } from "@/components/Clubs/ClubDrop";
 import { Oswald } from "next/font/google";
 
 const oswald = Oswald({ subsets: ["latin"] });
 
 const Clubs = () => {
+	const [key1, setKey1] = useState(false);
+	const [key2, setKey2] = useState(false);
+	const [key3, setKey3] = useState(false);
+	const [key4, setKey4] = useState(false);
+	// useEffect(() => {}, [key1, key2, key3, key4]);
+
 	return (
 		<div className="min-h-[60vh] w-80% flex justify-center mt-[15vh] mb-[15vh]">
 			<div className="container text-center flex flex-initial gap-[5vw]">
@@ -13,16 +20,57 @@ const Clubs = () => {
 				>
 					<div className="flex justify-center pl-4 text-[#FFF6E8] text-center">
 						<h1 className="text-4xl max-w-[25vw] text-left leading-[3.5rem] font-semibold">
-							FOUR ELEMENTS? <br /> FOUR DIRECTIONS? <br /> AND
-							THE FOUR OF US.
+							<span
+								className={"" + (key1 ? "text-[#FFCCDE]" : "")}
+							>
+								FOUR{" "}
+								<span
+									className={
+										"" + (key4 ? "text-[#F54E26]" : "")
+									}
+								>
+									ELEMENTS?
+								</span>
+							</span>
+							<br />{" "}
+							<span
+								className={"" + (key2 ? "text-[#007656]" : "")}
+							>
+								FOUR{" "}
+								<span
+									className={
+										"" + (key4 ? "text-[#F54E26]" : "")
+									}
+								>
+									{" "}
+									DIRECTIONS?
+								</span>
+							</span>
+							<br /> AND THE{" "}
+							<span
+								className={"" + (key3 ? "text-[#FABF29]" : "")}
+							>
+								FOUR OF US.
+							</span>
 						</h1>
 					</div>
 				</div>
 				<div className="basis-1/2 space-y-6">
-					<ClubDrop title="COMPUTER SOCIETY OF INDIA" />
-					<ClubDrop title="DEBATE SOCIETY" />
-					<ClubDrop title="BULLS AND BEARS" />
-					<ClubDrop title="ASSOCIATION OF COMPUTING MACHINERY" />
+					<div onClick={() => setKey1(!key1)}>
+						<ClubDrop title="COMPUTER SOCIETY OF INDIA" num={1} />
+					</div>
+					<div onClick={() => setKey2(!key2)}>
+						<ClubDrop title="DEBATE SOCIETY" num={2} />
+					</div>
+					<div onClick={() => setKey3(!key3)}>
+						<ClubDrop title="BULLS AND BEARS" num={3} />
+					</div>
+					<div onClick={() => setKey4(!key4)}>
+						<ClubDrop
+							title="ASSOCIATION OF COMPUTING MACHINERY"
+							num={4}
+						/>
+					</div>
 				</div>
 			</div>
 		</div>
