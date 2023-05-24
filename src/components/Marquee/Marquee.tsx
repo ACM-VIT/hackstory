4a15a022837c6a5885bdcd9c1598884a4d1abcc4
adityak840 <khandelwal.aditya840@gmail.com>
2023-05-24 18:32:41 +0530

    fix: changes

--- src/components/Marquee/Marquee.tsx
+++ src/components/Marquee/Marquee.tsx
@@ -7,29 +7,50 @@ const oswald = Oswald({subsets:['latin']})
 const Marquee = () => {
   return (
     <>
-      <div className="-translate-x-10 w-[110%] z-10 bg-logo relative flex overflow-x-hidden font-marquee mt-[23rem] -rotate-[4deg]">
+      <div className="-translate-x-10 w-[110%] z-10 bg-logo relative flex overflow-x-hidden font-marquee mt-[21rem] lg:mt-[23rem] -rotate-[4deg]">
         <div className="text-marquee lg:py-8 py-4 flex animate-marquee whitespace-nowrap">
-          <span className={`md:text-4xl font-bold text-2xl mx-6 ${oswald.className}`}>FOR YOURS TO BUILD</span>
-          <img src='/marquee-icon.svg' className='mx-6'></img>
-          <span className={`md:text-4xl font-bold text-2xl mx-6 ${oswald.className}`}>FOR YOURS TO BUILD</span>
-          <img src='/marquee-icon.svg' className='mx-6'></img>
-          <span className={`md:text-4xl font-bold text-2xl mx-6 ${oswald.className}`}>FOR YOURS TO BUILD</span>
-          <img src='/marquee-icon.svg' className='mx-6'></img>
-          <span className={`md:text-4xl font-bold text-2xl mx-6 ${oswald.className}`}>FOR YOURS TO BUILD</span>
-          <img src='/marquee-icon.svg' className='mx-6'></img>
-          <span className={`md:text-4xl font-bold text-2xl mx-6 ${oswald.className}`}>FOR YOURS TO BUILD</span>
+          <span className={`lg:text-4xl font-bold text-2xl gap-x-6 ml-3 flex flex-row ${oswald.className}`}>
+            <p className='lg:ml-3'>FOR YOURS TO BUILD</p>
+            <img src='/marquee-icon.svg' className='lg:mx-6 mr-6'></img>
+          </span>
+          <span className={`lg:text-4xl font-bold text-2xl gap-x-6 ml-3 flex flex-row ${oswald.className}`}>
+            <p className='lg:ml-3'>FOR YOURS TO BUILD</p>
+            <img src='/marquee-icon.svg' className='lg:mx-6 mr-6'></img>
+          </span>
+          <span className={`lg:text-4xl font-bold text-2xl gap-x-6 ml-3 flex flex-row ${oswald.className}`}>
+            <p className='lg:ml-3'>FOR YOURS TO BUILD</p>
+            <img src='/marquee-icon.svg' className='lg:mx-6 mr-6'></img>
+          </span>
+          <span className={`lg:text-4xl font-bold text-2xl gap-x-6 ml-3 flex flex-row ${oswald.className}`}>
+            <p className='lg:ml-3'>FOR YOURS TO BUILD</p>
+            <img src='/marquee-icon.svg' className='lg:mx-6 mr-6'></img>
+          </span>
+          <span className={`lg:text-4xl font-bold text-2xl lg:gap-x-3 gap-x-6 ml-3 flex flex-row ${oswald.className}`}>
+            <p className='lg:ml-3'>FOR YOURS TO BUILD</p>
+            <img src='/marquee-icon.svg' className='lg:mx-6 mr-6'></img>
+          </span>
         </div>
         <div className="text-marquee absolute top-0 lg:py-8 py-4 flex animate-marquee2 whitespace-nowrap font-marquee">
-          <img src='/marquee-icon.svg' className='mx-6'></img>
-          <span className={`md:text-4xl font-bold text-2xl mx-6 ${oswald.className}`}>FOR YOURS TO BUILD</span>
-          <img src='/marquee-icon.svg' className='mx-6'></img>
-          <span className={`md:text-4xl font-bold text-2xl mx-6 ${oswald.className}`}>FOR YOURS TO BUILD</span>
-          <img src='/marquee-icon.svg' className='mx-6'></img>
-          <span className={`md:text-4xl font-bold text-2xl mx-6 ${oswald.className}`}>FOR YOURS TO BUILD</span>
-          <img src='/marquee-icon.svg' className='mx-6'></img>
-          <span className={`md:text-4xl font-bold text-2xl mx-6 ${oswald.className}`}>FOR YOURS TO BUILD</span>
-          <img src='/marquee-icon.svg' className='mx-6'></img>
-          <span className={`md:text-4xl font-bold text-2xl mx-6 ${oswald.className}`}>FOR YOURS TO BUILD</span>
+          <span className={`lg:text-4xl font-bold text-2xl gap-x-6 ml-3 flex flex-row ${oswald.className}`}>
+            <p className='lg:ml-3'>FOR YOURS TO BUILD</p>
+            <img src='/marquee-icon.svg' className='lg:mx-6 mr-6'></img>
+          </span>
+          <span className={`lg:text-4xl font-bold text-2xl gap-x-6 ml-3 flex flex-row ${oswald.className}`}>
+            <p className='lg:ml-3'>FOR YOURS TO BUILD</p>
+            <img src='/marquee-icon.svg' className='lg:mx-6 mr-6'></img>
+          </span>
+          <span className={`lg:text-4xl font-bold text-2xl gap-x-6 ml-3 flex flex-row ${oswald.className}`}>
+            <p className='lg:ml-3'>FOR YOURS TO BUILD</p>
+            <img src='/marquee-icon.svg' className='lg:mx-6 mr-6'></img>
+          </span>
+          <span className={`lg:text-4xl font-bold text-2xl gap-x-6 ml-3 flex flex-row ${oswald.className}`}>
+            <p className='lg:ml-3'>FOR YOURS TO BUILD</p>
+            <img src='/marquee-icon.svg' className='lg:mx-6 mr-6'></img>
+          </span>
+          <span className={`lg:text-4xl font-bold text-2xl gap-x-6 ml-3 flex flex-row ${oswald.className}`}>
+            <p className='lg:ml-3'>FOR YOURS TO BUILD</p>
+            <img src='/marquee-icon.svg' className='lg:mx-6 mr-6'></img>
+          </span>
         </div>
       </div>
       <div className="-translate-x-10 w-[110%] z-1 mt-[-65px] rotate-[6deg] w-contain h-full lg:py-12 py-6 bg-cross"></div>
