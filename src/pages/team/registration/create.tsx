973e85c09280f0066358ca04484027e6ff356ec9
adityak840 <khandelwal.aditya840@gmail.com>
2023-06-01 08:31:57 +0530

    feat: new registration pages

--- src/pages/team/registration/create.tsx
+++ src/pages/team/registration/create.tsx
@@ -1,8 +1,24 @@
+import { Manrope,Oswald } from 'next/font/google'
 import React from 'react'
+const manrope = Manrope({subsets: ['latin']})
+const oswald = Oswald({subsets:['latin']})
 
 const create = () => {
   return (
-    <div>create</div>
+    <div className='flex flex-col gap-y-12'>
+        <div className={`bg-title text-black w-1/5 ml-36 mt-52 pl-16 py-4 rounded-[10px] font-bold text-xl ${manrope.className}`}>
+            Hey, Participant
+        </div>
+        <div className='bg-title ml-36 mr-[40rem] rounded-[20px] h-[20rem]'>
+            <div className="border-b border-yellow py-4 px-20 text-[20px] font-bold flex justify-between">
+                <p className={`${oswald.className} py-1`}>WELCOME TO HACKSTORY REGISTRATION</p>
+                <a className={`${manrope.className} border-yellow border px-8 py-1 rounded-[30px]`}>CANCEL</a>
+            </div>
+            <div className={`ml-16 ${manrope.className} font-bold pt-[2rem] text-xl`}>
+                Enter your team name
+            </div>
+        </div>
+    </div>
   )
 }
 
--- src/sections/Register-Sections/Team.tsx
+++ src/sections/Register-Sections/Team.tsx
@@ -3,9 +3,11 @@ import React, { useState } from "react";
 import { useRouter } from "next/router";
 import Toaster from "@/utils/toaster";
 import { DEV_BASE_URL } from "@/constants";
-import { Manrope } from "next/font/google";
+import { Manrope,Oswald } from "next/font/google";
+import Header from "@/components/Common/Header";
 
 const manrope = Manrope({subsets:['latin']})
+const oswald = Oswald({subsets:['latin']})
 
 const style = {
   position: 'absolute' as 'absolute',
@@ -83,37 +85,41 @@ const Registration = () => {
   };
 
   return (
-    <div className="flex flex-col h-contain mt-[10%] gap-y-12">
-      <div className="flex flex-col">
-        <h1 className="text-white lg:text-5xl text-3xl font-bold sm:text-center text-left pb-2 px-12">
-          <span className="pb-8 block">
-            <span className="text-yellow">HACK YOUR WAY{" "}</span>
-            THROUGH A WORLD,
-          </span>
-          <span className="text-white">WHERE INNOVATION WRITES YOUR</span>
-          <span className="text-yellow"> NEXT CHAPTER</span>
-        </h1>
-      </div>
-      <div className="mx-[25%]">
-        <div className="flex md:flex-row flex-col items-center justify-center border border-white rounded-3xl gap-x-16 gap-y-8 py-20 border-opacity-10">
-          <button
-            onClick={() => {
-              router.push("/team/registration/join");
-            }}
-            className="flex cursor-pointer flex-row px-8 py-4 rounded-[30px] text-white text-[15px] lg:text-[20px] border border-yellow">
-            JOIN A TEAM
-          </button>
-          <div className="hidden md:block h-8 sm:h-12 md:h-16 w-1 bg-gray-400"></div>
-          <button
-            onClick={() => {
-              router.push("/team/registration/create");
-            }}
-            className="flex align-center cursor-pointer px-8 py-4 rounded-[30px] bg-yellow text-[12px] sm:text-[15px] lg:text-[20px]">
-            <p>CREATE A TEAM</p>
-          </button>
+    <>
+      <Header />
+      <div className="flex flex-col h-contain mt-[10%] gap-y-12">
+        <div className="flex flex-col">
+          <h1 className={`text-white lg:text-5xl text-3xl font-bold sm:text-center text-left pb-2 px-12 ${oswald.className}`}>
+            <span className="pb-8 block">
+              <span className="text-yellow">HACK YOUR WAY{" "}</span>
+              THROUGH A WORLD,
+            </span>
+            <span className="text-white">WHERE INNOVATION WRITES YOUR</span>
+            <span className="text-yellow"> NEXT CHAPTER</span>
+          </h1>
+        </div>
+        <div className="mx-[25%]">
+          <div className="flex md:flex-row flex-col items-center justify-center border border-white rounded-3xl gap-x-16 gap-y-8 py-20 border-opacity-10">
+            <button
+              onClick={() => {
+                router.push("/team/registration/join");
+              }}
+              className={`flex cursor-pointer flex-row px-8 py-4 rounded-[30px] text-white text-[15px] lg:text-[20px] border border-yellow font-bold ${manrope.className}`}>
+              JOIN A TEAM
+            </button>
+            <div className="hidden md:block h-8 sm:h-12 md:h-16 w-1 bg-gray-400"></div>
+            <button
+              onClick={() => {
+                router.push("/team/registration/create");
+              }}
+              className={`flex align-center cursor-pointer px-8 py-4 rounded-[30px] bg-yellow text-[12px] sm:text-[15px] lg:text-[20px] font-bold ${manrope.className}`}>
+              <p>CREATE A TEAM</p>
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
+    
 
   );
 };
