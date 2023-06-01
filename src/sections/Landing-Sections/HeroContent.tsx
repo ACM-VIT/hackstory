54ee04416067637a35ebcfa34110f2b199b690d8
Arjun Khanna <arjunkhanna0108@gmail.com>
2023-06-01 16:20:43 +0530

    feat: doodle animation for hero

--- src/sections/Landing-Sections/HeroContent.tsx
+++ src/sections/Landing-Sections/HeroContent.tsx
@@ -5,6 +5,9 @@ import { DEV_BASE_URL } from "@/constants";
 import getHandler from "@/handlers/getHandler";
 import { useRouter } from "next/router";
 import { Alfa_Slab_One, Manrope } from "next/font/google";
+import Image from "next/image";
+import { motion } from "framer-motion";
+import { lookup } from "dns";
 
 const manrope = Manrope({ subsets: ["latin"] });
 const alfaSlabOne = Alfa_Slab_One({
@@ -36,25 +39,46 @@ const HeroContent = () => {
 	};
 
 	return (
-		<div className="flex flex-col items-center justify-center px-16 gap-y-6  pt-36 max-md:pt-16">
+		<div className="flex flex-col items-center justify-center gap-y-6 px-16  pt-36 max-md:pt-16">
 			<div className="flex flex-col gap-y-4">
 				<div
-					className={`text-timelineheading text-md font-bold md:text-xl lg:text-2xl ${manrope.className} text-tracks max-lg:text-center`}
+					className={`text-timelineheading z-9 text-md font-bold md:text-xl lg:text-2xl ${manrope.className} text-tracks max-lg:text-center`}
 				>
 					JUNE 5TH-7TH 2023
 				</div>
+
 				<div
-					className={`text-[40px] font-bold md:text-[86px] lg:text-[90px] ${alfaSlabOne.className} text-title max-lg:text-center`}
+					className={`text-[40px] font-bold md:text-[86px] lg:text-[90px] ${alfaSlabOne.className} z-9 text-title max-lg:text-center`}
 				>
 					HACK STORY
 				</div>
+				<motion.div
+					initial={{ opacity: 0, scale: 0.75 }}
+					animate={{ opacity: 1, scale: 1 }}
+					transition={{
+						duration: 3,
+						repeat: Infinity,
+						repeatType: "reverse",
+					}}
+					className="absolute left-[10vw] max-lg:mt-[5vh]"
+				>
+					<Image
+						className="w-[150px] max-lg:w-[75px]"
+						src="/idea-icon.svg"
+						width={10000}
+						height={10000}
+						alt="idea-icon"
+					/>
+				</motion.div>
 			</div>
-			<div className={`text-timelineheading text-center text-sm font-semibold md:text-lg lg:text-xl ${manrope.className} text-[#E8E8EE]`}>
-				Hack your way through the world, where innovation writes the next&nbsp;
+			<div
+				className={`text-timelineheading text-center text-sm font-semibold md:text-lg lg:text-xl ${manrope.className} text-[#E8E8EE]`}
+			>
+				Hack your way through the world, where innovation writes the
+				next&nbsp;
 				<br className="hidden sm:block" />
 				chapter!
 			</div>
-
 
 			{session ? (
 				isPartOfTeam ? (
@@ -72,15 +96,14 @@ const HeroContent = () => {
 						JOIN TEAM
 					</div>
 				)
-				) : (
+			) : (
 				<div
 					className={`mt-12 flex cursor-pointer flex-row rounded-[30px] bg-yellow px-8 py-4 text-[12px] font-extrabold md:text-[15px] lg:text-[20px] ${manrope.className}`}
 					onClick={() => signIn()}
 				>
 					REGISTER NOW
 				</div>
-				)}
-
+			)}
 		</div>
 	);
 };
