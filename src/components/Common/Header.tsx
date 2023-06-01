3e4e1491fa1da5b06a766dad14365efdb53927ac
Arjun Khanna <arjunkhanna0108@gmail.com>
2023-06-01 07:37:26 +0530

    feat: animations for navbar

--- src/components/Common/Header.tsx
+++ src/components/Common/Header.tsx
@@ -1,126 +1,199 @@
 import Link from "next/link";
 import React, { useState } from "react";
+import { motion, Variants } from "framer-motion";
+
+const navVariants = {
+	open: {
+		transition: { staggerChildren: 0.07, delayChildren: 0.2 },
+	},
+	closed: {
+		transition: { staggerChildren: 0.05, staggerDirection: -1 },
+	},
+};
+
+const liVariants = {
+	open: {
+		y: 0,
+		opacity: 1,
+		transition: {
+			y: { stiffness: 1000, velocity: -100 },
+		},
+	},
+	closed: {
+		y: 50,
+		opacity: 0,
+		transition: {
+			y: { stiffness: 1000 },
+		},
+	},
+};
+
+const Path = (props: any) => (
+	<motion.path
+		fill="transparent"
+		strokeWidth="3"
+		stroke="#fff"
+		strokeLinecap="round"
+		{...props}
+	/>
+);
 
 const Header = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const handleMenuToggle = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
+	const handleMenuToggle = () => {
+		setIsMenuOpen(!isMenuOpen);
+	};
 
-  return (
-    <div className="mx-auto px-8 py-10 lg:border-b-[1px] border-[#2E2E2E]">
-      <div className="flex flex-col justify-center lg:flex-row lg:items-center">
-        <div className="flex flex-row items-center">
-          {isMenuOpen ? (
-            <svg
-              className="lg:hidden"
-              xmlns="http://www.w3.org/2000/svg"
-              x="0px"
-              y="0px"
-              width="24"
-              height="24"
-              viewBox="0 0 50 50"
-              onClick={handleMenuToggle}
-            >
-              <path
-                d="M 9.15625 6.3125 L 6.3125 9.15625 L 22.15625 25 L 6.21875 40.96875 L 9.03125 43.78125 L 25 27.84375 L 40.9375 43.78125 L 43.78125 40.9375 L 27.84375 25 L 43.6875 9.15625 L 40.84375 6.3125 L 25 22.15625 Z"
-                fill="#FFFFFF"
-              ></path>
-            </svg>
-          ) : (
-            <svg
-              className="lg:hidden"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              width="24px"
-              height="24px"
-              onClick={handleMenuToggle}
-            >
-              <path
-                d="M 2 5 L 2 7 L 22 7 L 22 5 L 2 5 z M 2 11 L 2 13 L 22 13 L 22 11 L 2 11 z M 2 17 L 2 19 L 22 19 L 22 17 L 2 17 z"
-                fill="#FFFFFF"
-              />
-            </svg>
-          )}
-        </div>
-        <nav
-          className={`transition-all duration-500 ease-in-out lg:block ${
-            isMenuOpen ? "" : "hidden"
-          }`}
-        >
-          <ul
-            className={`mt-4 flex flex-col items-center gap-y-4 lg:mt-0 lg:flex-row lg:space-x-12`}
-          >
-            <li>
-              <Link href="/" 
-                className="text-l font-extrabold text-headerbutton hover:text-gray-900">
-              
-                HOME</Link>
-                
-              
-            </li>
-            <li>
-              <a
-                href="#"
-                className="text-l font-extrabold text-headerbutton hover:text-gray-900"
-              >
-                PRIZES
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="text-l font-extrabold text-headerbutton hover:text-gray-900"
-              >
-                TRACKS
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="text-l font-extrabold text-headerbutton hover:text-gray-900"
-              >
-                TIMELINE
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="text-l font-extrabold text-headerbutton hover:text-gray-900"
-              >
-                ABOUT HACK
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="text-l font-extrabold text-headerbutton hover:text-gray-900"
-              >
-                SPONSORS
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="text-l font-extrabold text-headerbutton hover:text-gray-900"
-              >
-                ABOUT CLUBS
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="text-l font-extrabold text-headerbutton hover:text-gray-900"
-              >
-                FAQs
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </div>
-  );
+	return (
+		<motion.div
+			initial={false}
+			animate={isMenuOpen ? "open" : "closed"}
+			className="mx-auto border-[#2E2E2E] px-8 py-10 lg:border-b-[1px]"
+		>
+			<div className="flex flex-col justify-center lg:flex-row lg:items-center">
+				<div className="">
+					<svg
+						onClick={handleMenuToggle}
+						className="lg:hidden"
+						width="24px"
+						height="24px"
+						viewBox="0 0 23 23"
+					>
+						<Path
+							variants={{
+								closed: { d: "M 2 2.5 L 20 2.5" },
+								open: { d: "M 3 16.5 L 17 2.5" },
+							}}
+							transition={{ duration: 0.5 }}
+						/>
+						<Path
+							d="M 2 9.423 L 20 9.423"
+							variants={{
+								closed: { opacity: 1 },
+								open: { opacity: 0 },
+							}}
+							transition={{ duration: 0.1 }}
+						/>
+						<Path
+							variants={{
+								closed: { d: "M 2 16.346 L 20 16.346" },
+								open: { d: "M 3 2.5 L 17 16.346" },
+							}}
+							transition={{ duration: 0.5 }}
+						/>
+					</svg>
+				</div>
+				<nav
+					className={`transition-all duration-500 ease-in-out lg:block ${
+						isMenuOpen ? "" : "hidden"
+					}`}
+				>
+					<motion.ul
+						variants={navVariants}
+						className={`mt-4 flex flex-col items-center gap-y-4 lg:mt-0 lg:flex-row lg:space-x-12`}
+					>
+						<motion.li
+							variants={liVariants}
+							whileHover={{ scale: 1.1 }}
+							whileTap={{ scale: 0.95 }}
+						>
+							<Link
+								href="/"
+								className="text-l font-extrabold text-headerbutton hover:text-gray-900"
+							>
+								HOME
+							</Link>
+						</motion.li>
+						<motion.li
+							variants={liVariants}
+							whileHover={{ scale: 1.1 }}
+							whileTap={{ scale: 0.95 }}
+						>
+							<a
+								href="#"
+								className="text-l font-extrabold text-headerbutton hover:text-gray-900"
+							>
+								PRIZES
+							</a>
+						</motion.li>
+						<motion.li
+							variants={liVariants}
+							whileHover={{ scale: 1.1 }}
+							whileTap={{ scale: 0.95 }}
+						>
+							<a
+								href="#"
+								className="text-l font-extrabold text-headerbutton hover:text-gray-900"
+							>
+								TRACKS
+							</a>
+						</motion.li>
+						<motion.li
+							variants={liVariants}
+							whileHover={{ scale: 1.1 }}
+							whileTap={{ scale: 0.95 }}
+						>
+							<a
+								href="#"
+								className="text-l font-extrabold text-headerbutton hover:text-gray-900"
+							>
+								TIMELINE
+							</a>
+						</motion.li>
+						<motion.li
+							variants={liVariants}
+							whileHover={{ scale: 1.1 }}
+							whileTap={{ scale: 0.95 }}
+						>
+							<a
+								href="#"
+								className="text-l font-extrabold text-headerbutton hover:text-gray-900"
+							>
+								ABOUT HACK
+							</a>
+						</motion.li>
+						<motion.li
+							variants={liVariants}
+							whileHover={{ scale: 1.1 }}
+							whileTap={{ scale: 0.95 }}
+						>
+							<a
+								href="#"
+								className="text-l font-extrabold text-headerbutton hover:text-gray-900"
+							>
+								SPONSORS
+							</a>
+						</motion.li>
+						<motion.li
+							variants={liVariants}
+							whileHover={{ scale: 1.1 }}
+							whileTap={{ scale: 0.95 }}
+						>
+							<a
+								href="#"
+								className="text-l font-extrabold text-headerbutton hover:text-gray-900"
+							>
+								ABOUT CLUBS
+							</a>
+						</motion.li>
+						<motion.li
+							variants={liVariants}
+							whileHover={{ scale: 1.1 }}
+							whileTap={{ scale: 0.95 }}
+						>
+							<a
+								href="#"
+								className="text-l font-extrabold text-headerbutton hover:text-gray-900"
+							>
+								FAQs
+							</a>
+						</motion.li>
+					</motion.ul>
+				</nav>
+			</div>
+		</motion.div>
+	);
 };
 
 export default Header;
