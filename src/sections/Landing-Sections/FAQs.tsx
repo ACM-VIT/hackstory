5ed62888e34cce2febc9b9269fa28d03a5ccf0ae
Arjun Khanna <arjunkhanna0108@gmail.com>
2023-06-03 19:49:33 +0530

    fix: faq dropdown button animation

--- src/sections/Landing-Sections/FAQs.tsx
+++ src/sections/Landing-Sections/FAQs.tsx
@@ -44,15 +44,15 @@ const FAQPage = () => {
 			answer: "Your laptop, teammates and perhaps some coffee. Also, the narcissism of a winner.",
 		},
 		{
-			question:"What is the event flow of the hackathon?",
+			question: "What is the event flow of the hackathon?",
 			answer: "Head over to the timeline section for more details.",
 		},
 		{
-			question:"Whom should I contact for doubts and queries?",
+			question: "Whom should I contact for doubts and queries?",
 			answer: "Use the discord server to get in touch with our team for any queries.",
 		},
 		{
-			question:"When and where is the hack?",
+			question: "When and where is the hack?",
 			answer: "HackStory’23 will be conducted from 4th June-6th June starting at 4 PM at Anna Auditorium.",
 		},
 	];
@@ -98,30 +98,71 @@ const FAQPage = () => {
 							<motion.button
 								whileTap={{ scale: 0.97 }}
 								className="flex w-full items-center justify-between focus:outline-none"
-								
 							>
 								<span className="pr-4 text-left text-base font-semibold text-white sm:text-lg">
 									{faq.question}
 								</span>
 								<motion.span
-									variants={{
-										open: { rotate: 180 },
-										closed: { rotate: 0 },
-									}}
+									variants={
+										{
+											// open: { rotate: 180 },
+											// closed: { rotate: 0 },
+										}
+									}
 									transition={{ duration: 0.2 }}
 									style={{ originY: 0.55 }}
-									className="sm:text-2xl text-lg text-white"
+									className="text-lg text-white sm:text-2xl"
 								>
-									{activeIndex === index ? "-" : "+"}
+									{activeIndex === index ? (
+										<motion.div
+											variants={{
+												open: { rotate: 180 },
+												closed: { rotate: 0 },
+											}}
+											transition={{ duration: 0.2 }}
+											style={{ originY: 0.55 }}
+											className="white"
+										>
+											<svg
+												fill="#A7A9BE"
+												width="15"
+												height="15"
+												viewBox="0 0 20 20"
+											>
+												<path d="M0 7 L 20 7 L 10 16" />
+											</svg>
+										</motion.div>
+									) : (
+										<motion.div
+											variants={
+												{
+													// open: { rotate: 180 },
+													// closed: { rotate: 0 },
+												}
+											}
+											transition={{ duration: 0.2 }}
+											style={{ originY: 0.55 }}
+											className="white"
+										>
+											<svg
+												fill="#A7A9BE"
+												width="15"
+												height="15"
+												viewBox="0 0 20 20"
+											>
+												<path d="M0 7 L 20 7 L 10 16" />
+											</svg>
+										</motion.div>
+									)}
 								</motion.span>
 							</motion.button>
-								{activeIndex === index && (
-									<div className={`mt-2 pt-2`}>
-										<p className="text-30px mt-4 text-white ">
-											{faq.answer}
-										</p>
-									</div>
-								)}
+							{activeIndex === index && (
+								<div className={`mt-2 pt-2`}>
+									<p className="text-30px mt-4 text-white ">
+										{faq.answer}
+									</p>
+								</div>
+							)}
 						</motion.div>
 					))}
 				</div>
