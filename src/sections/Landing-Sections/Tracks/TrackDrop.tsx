53319d3987e444374941a5c71671b767fe5a6445
Arjun Khanna <arjunkhanna0108@gmail.com>
2023-06-03 19:46:27 +0530

    feat: changed dropdown icon in track section

--- src/sections/Landing-Sections/Tracks/TrackDrop.tsx
+++ src/sections/Landing-Sections/Tracks/TrackDrop.tsx
@@ -7,72 +7,86 @@ const manrope = Manrope({ subsets: ["latin"] });
 const oswald = Oswald({ subsets: ["latin"] });
 
 interface Track {
-  name: string;
+	name: string;
 }
 
 interface Props {
-  title: string;
-  answer: string;
+	title: string;
+	answer: string;
 }
 
 export const TrackDrop = ({ title, answer }: Props) => {
-  const router = useRouter();
-  const [open, setOpen] = useState(false);
+	const router = useRouter();
+	const [open, setOpen] = useState(false);
 
-  const toggleAccordion = () => {
-    setOpen(!open);
-  };
+	const toggleAccordion = () => {
+		setOpen(!open);
+	};
 
-  const getIcon = () => {
-    if (open) {
-      return (
-        <svg fill="#A7A9BE" width="15" height="15" viewBox="0 0 20 20" style={{ transform: 'rotate(180deg)' }}>
-          <path d="M0 7 L 20 7 L 10 13" />
-        </svg>
-      );
-    } else {
-      return (
-        <svg fill="#A7A9BE" width="15" height="15" viewBox="0 0 20 20">
-          <path d="M0 7 L 20 7 L 10 13" />
-        </svg>
-      );
-    }
-  };
-    return (
-      <div>
-        <div className="text-left">
-          <div className="rounded border-b border-t border-gray-300 border-opacity-10 px-4 py-6">
-            <button className={`flex w-full items-center justify-between focus:outline-none ${manrope.className}`}>
-              <span className="pr-4 text-left text-lg font-semibold text-white sm:text-xl">{title}</span>
-            </button>
-            <p className={`md:mr-20 pt-4 text-[#FFF6E8] ${manrope.className}`} dangerouslySetInnerHTML={{ __html: answer }}></p>
-          </div>
-        </div>
-      </div>
-    );
+	const getIcon = () => {
+		return (
+			<motion.div
+				variants={{
+					open: { rotate: 180 },
+					closed: { rotate: 0 },
+				}}
+				transition={{ duration: 0.2 }}
+				style={{ originY: 0.55 }}
+				className="white"
+			>
+				<svg fill="#A7A9BE" width="15" height="15" viewBox="0 0 20 20">
+					<path d="M0 7 L 20 7 L 10 16" />
+				</svg>
+			</motion.div>
+		);
+	};
+	return (
+		<div>
+			<div className="text-left">
+				<div className="rounded border-b border-t border-gray-300 border-opacity-10 px-4 py-6">
+					<button
+						className={`flex w-full items-center justify-between focus:outline-none ${manrope.className}`}
+					>
+						<span className="pr-4 text-left text-lg font-semibold text-white sm:text-xl">
+							{title}
+						</span>
+					</button>
+					<p
+						className={`pt-4 text-[#FFF6E8] md:mr-20 ${manrope.className}`}
+						dangerouslySetInnerHTML={{ __html: answer }}
+					></p>
+				</div>
+			</div>
+		</div>
+	);
 
-  return (
-    <div>
-      <div className="text-left">
-        <motion.div
-          initial={false}
-          animate={open ? "open" : "closed"}
-          className="rounded border-b border-t border-gray-300 border-opacity-10 px-4 py-6"
-          onClick={toggleAccordion}
-        >
-          <button className={`flex w-full items-center justify-between focus:outline-none ${manrope.className}`}>
-            <span className={`pr-4 text-left text-lg font-semibold text-white sm:text-xl`}>{title}</span>
-            {getIcon()}
-          </button>
-          {open && (
-            <motion.p
-              className={`mr-20 pt-4 text-[#FFF6E8] ${manrope.className}`}
-              dangerouslySetInnerHTML={{ __html: answer }}
-            ></motion.p>
-          )}
-        </motion.div>
-      </div>
-	  
-    </div>	
-  );
+	// return (
+	// 	<div>
+	// 		<div className="text-left">
+	// 			<motion.div
+	// 				initial={false}
+	// 				animate={open ? "open" : "closed"}
+	// 				className="rounded border-b border-t border-gray-300 border-opacity-10 px-4 py-6"
+	// 				onClick={toggleAccordion}
+	// 			>
+	// 				<button
+	// 					className={`flex w-full items-center justify-between focus:outline-none ${manrope.className}`}
+	// 				>
+	// 					<span
+	// 						className={`pr-4 text-left text-lg font-semibold text-white sm:text-xl`}
+	// 					>
+	// 						{title}
+	// 					</span>
+	// 					{getIcon()}
+	// 				</button>
+	// 				{open && (
+	// 					<motion.p
+	// 						className={`mr-20 pt-4 text-[#FFF6E8] ${manrope.className}`}
+	// 						dangerouslySetInnerHTML={{ __html: answer }}
+	// 					></motion.p>
+	// 				)}
+	// 			</motion.div>
+	// 		</div>
+	// 	</div>
+	// );
 };
