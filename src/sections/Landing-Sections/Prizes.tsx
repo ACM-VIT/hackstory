6510b03967279cdd6b2923faf61802584ae5317b
adityak840 <khandelwal.aditya840@gmail.com>
2023-06-05 11:06:21 +0530

    feat: prizes section added

--- src/sections/Landing-Sections/Prizes.tsx
+++ src/sections/Landing-Sections/Prizes.tsx
@@ -1,4 +1,6 @@
+import { Manrope } from "next/font/google";
 import React from "react";
+const manrope = Manrope({subsets:['latin']})
 
 const Prizes = () => {
   return (
@@ -9,11 +11,28 @@ const Prizes = () => {
         PRIZES
         </text>
       </div>
-      <p
-        className={`pb-48 md:pb-60 lg:pb-64 text-center text-white text-[1rem] md:text-[1.25rem] lg:text-[1.75rem] font-semibold`}
-      >
-        Will be informed soon..
-      </p>
+      <div className={`flex-wrap gap-20 grid grid-cols-6 max-lg:grid-cols-1 align-center justify-center ${manrope.className}`}>
+        <div className="bg-[#2E2E2E] w-[18rem] h-[8rem] lg:col-start-1 lg:col-end-3 flex flex-col items-center justify-center rounded-[20px]">
+          <p className="text-[#A7A9BE]">FIRST PRIZE</p>
+          <p className="text-[#D0D5FF] font-bold text-[32px]">₹ 8,000</p>
+        </div>
+        <div className="bg-[#2E2E2E] w-[18rem] h-[8rem] lg:col-start-3 lg:col-end-5 flex flex-col items-center justify-center rounded-[20px]">
+          <p className="text-[#A7A9BE]">SECOND PRIZE</p>
+          <p className="text-[#D0D5FF] font-bold text-[32px]">₹ 5,000</p>
+        </div>
+        <div className="bg-[#2E2E2E] w-[18rem] h-[8rem] lg:col-start-5 lg:col-end-7 flex flex-col items-center justify-center rounded-[20px]">
+          <p className="text-[#A7A9BE]">THIRD PRIZE</p>
+          <p className="text-[#D0D5FF] font-bold text-[32px]">₹ 3,000</p>
+        </div>
+        <div className="bg-[#2E2E2E] w-[18rem] h-[8rem] lg:col-start-2 lg:col-end-4 flex flex-col  items-self-center items-center justify-center rounded-[20px]">
+          <p className="text-[#A7A9BE]">BEST UI/UX</p>
+          <p className="text-[#D0D5FF] font-bold text-[32px]">₹ 2,000</p>
+        </div>
+        <div className="bg-[#2E2E2E] w-[18rem] h-[8rem] lg:col-start-4 lg:col-end-6 flex flex-col items-center justify-center rounded-[20px]">
+          <p className="text-[#A7A9BE]">BEST FRESHERS</p>
+          <p className="text-[#D0D5FF] font-bold text-[32px]">₹ 2,000</p>
+        </div>
+      </div>
     </div>
   );
 };
