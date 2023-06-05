93c7e7822bfffd3a70f0a8372f18e406eef00fb4
Arjun Khanna <arjunkhanna0108@gmail.com>
2023-06-05 22:23:27 +0530

    feat: play section added

---
+++ src/pages/play.tsx
@@ -0,0 +1,14 @@
+import Header from "@/components/Common/Header";
+import React from "react";
+import PlaySection from "@/sections/Play-Section/PlaySection";
+
+const Play = () => {
+	return (
+		<div>
+			<Header />
+			<PlaySection />
+		</div>
+	);
+};
+
+export default Play;
